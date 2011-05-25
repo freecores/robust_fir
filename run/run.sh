eedcbf2dde3c645d7a5caee6507535fa8547c3ae
eyalhoc <eyalhoc@a6c964eb-1eb3-47ba-865b-cfbdd48b396f>
2011-05-25 16:36:45 +0000

    GUI support

--- run/run.sh
+++ run/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-../../../robust ../src/base/fir.v ../src/base/def_fir_top.txt -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
+../../../robust -ver
+if [ $? -eq 0 ];then
+  ROBUST=../../../robust
+else
+  echo "RobustVerilog warning: GUI version not supported - using non-GUI version robust-lite"
+  ROBUST=../../../robust-lite
+fi
+
+$ROBUST ../src/base/fir.v ../src/base/def_fir_top.txt -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
--- src/base/fir.v
+++ src/base/fir.v
@@ -49,32 +49,33 @@ ITER CX COEFF_NUM
 //    Architecture              = ARCH
 //    Sum of Products Latency   = LATENCY
 
+
 module fir_NAME (PORTS);
 			
-	input  clk;
-	input  reset;
-	input  [EXPR(COEFF_BITS-1):0] kCX;
-	input  [EXPR(DIN_BITS-1):0] data_in;
-	output [EXPR(DOUT_BITS-1):0] data_out;
-    input  valid_in;
-	output valid_out;
-
+   input                            clk;
+   input                            reset;
+   input [EXPR(COEFF_BITS-1):0]     kCX;
+   input [EXPR(DIN_BITS-1):0]       data_in;
+   output [EXPR(DOUT_BITS-1):0]     data_out;
+   input                            valid_in;
+   output                           valid_out;
+   
 	
-IFDEF TRUE(MAC_NUM==1)
+IFDEF MAC_EQ(1)
   CREATE fir_serial.v def_fir_basic.txt DEFCMD(SWAP CONST(ORDER) ORDER) DEFCMD(SWAP CONST(COEFF_BITS) COEFF_BITS) DEFCMD(SWAP CONST(DIN_BITS) DIN_BITS)
   fir_serial_TOPO fir(clk, reset, valid_in, CONCAT.REV(kCX ,), data_in, data_out, valid_out);
   
-ELSE TRUE(MAC_NUM==1)
-  IFDEF TRUE(COEFF_NUM==MAC_NUM)
+ELSE MAC_EQ(1)
+  IFDEF MAC_EQ(COEFF_NUM)
   CREATE fir_parallel.v def_fir_basic.txt DEFCMD(SWAP CONST(ORDER) ORDER) DEFCMD(SWAP CONST(COEFF_BITS) COEFF_BITS) DEFCMD(SWAP CONST(DIN_BITS) DIN_BITS)
   fir_parallel_TOPO fir(clk, reset, valid_in, CONCAT.REV(kCX ,), data_in, data_out, valid_out);
   
-  ELSE TRUE(COEFF_NUM==MAC_NUM)
+  ELSE MAC_EQ(COEFF_NUM)
   CREATE fir_Nserial.v def_fir_Nserial.txt DEFCMD(SWAP CONST(ORDER) ORDER) DEFCMD(SWAP CONST(COEFF_BITS) COEFF_BITS) DEFCMD(SWAP CONST(DIN_BITS) DIN_BITS) DEFCMD(SWAP CONST(MAC_NUM) MAC_NUM)
   fir_MAC_NUMserial_TOPO fir(clk, reset, valid_in, CONCAT.REV(kCX ,), data_in, data_out, valid_out);
   
-  ENDIF TRUE(COEFF_NUM==MAC_NUM)
-ENDIF TRUE(MAC_NUM==1)
+  ENDIF MAC_EQ(COEFF_NUM)
+ENDIF MAC_EQ(1)
   	
 	
 endmodule
